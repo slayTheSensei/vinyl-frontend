6dc65b8bb3fd9570e338cfd34916085d10235aeb
slayTheSensei <kylejpearson@gmail.com>
2017-10-05 01:57:25 -0400

    STARTED DASHBOARD DESIGN
    Started creating the bones of artist page. still need to seperate
    out the components once the skeleton is done.

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,13 +1,59 @@
-import React, { Component } from 'react';
-import axios from 'axios'
-import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
-import  { Link, Redirect, Route} from 'react-router-dom'
+import React, { Component } from 'react'
+import { Sidebar, Segment, Button, Menu, Image, Icon, Header, Grid, Card } from 'semantic-ui-react'
 
 class Dashboard extends Component {
+  state = { visible: true }
+
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+
   render() {
-    return(
-      <div>
-        <h1>Dashboard!</h1>
+    const { visible } = this.state
+    return (
+      <div className="app">
+        <Button onClick={this.toggleVisibility}>Toggle Visibility</Button>
+        <Sidebar.Pushable as={Segment} >
+          <Sidebar as={Menu} animation='uncover' width='thin' visible={visible} icon='labeled' vertical inverted>
+            <Menu.Item name='home'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item name='gamepad'>
+              <Icon name='gamepad' />
+              Games
+            </Menu.Item>
+            <Menu.Item name='camera'>
+              <Icon name='camera' />
+              Channels
+            </Menu.Item>
+          </Sidebar>
+          <Sidebar.Pusher className="side">
+            <Segment basic>
+              <Header as='h2'>All Available Artists</Header>
+                <Card>
+                  <Image src='https://react.semantic-ui.com/assets/images/avatar/large/matthew.png' />
+                  <Card.Content>
+                    <Card.Header>
+                      Matthew
+                    </Card.Header>
+                    <Card.Meta>
+                      <span className='date'>
+                        Joined in 2015
+                      </span>
+                    </Card.Meta>
+                    <Card.Description>
+                      Matthew is a musician living in Nashville.
+                    </Card.Description>
+                  </Card.Content>
+                  <Card.Content extra>
+                    <a>
+                      <Icon name='user' />
+                      22 Friends
+                    </a>
+                  </Card.Content>
+                </Card>
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
       </div>
     )
   }
